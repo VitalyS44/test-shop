590a9c724728c2fa2a5a1ede530e8148afebffbe
Vitaliy <vitaliysergienko44@gmail.com>
2021-05-27 23:21:57 +0300

    fix config

---
+++ config/webpack.dev.js
@@ -0,0 +1,37 @@
+const webpack = require('webpack')
+const { merge } = require('webpack-merge')
+const path = require('path')
+const common = require('./webpack.common.js')
+
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: 'inline-source-map',
+
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    open: true,
+    hot: true,
+    port: 3000,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(less|css)$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: { sourceMap: true, importLoaders: 1, modules: true },
+          },
+          { loader: 'postcss-loader', options: { sourceMap: true } },
+          { loader: 'less-loader', options: { sourceMap: true } },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+})
